5506412a7391fa090887951e318cb20dd1ccafc7
leo-gith <130533854+leo-gith@users.noreply.github.com>
2023-04-24 21:30:08 +0530

    Add files via upload

---
+++ bottles.js
@@ -0,0 +1,46 @@
+img="";
+status="";
+
+function setup()
+{
+    canvas = createCanvas(700,600);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : detecting Objects";
+}
+
+function preload()
+{
+    img=loadImage("bottle.png");
+}
+
+function draw()
+{
+    image(img,0,0,700,600);
+    fill("#ff0000")
+    text("Bottles",35,60);
+    noFill();
+    stroke("#FF0000"); 
+    rect(30,60,750,600);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
+
+function back()
+{
+    window.location="iindex.html";
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function bedroom()
+{
+    window.location="bedroon.html";
+}
+
+function Tv_ac()
+{
+    window.location="Tv_ac.html";
+}
+
+function desk()
+{
+    window.location="desk.html";
+}
+
+function bottles()
+{
+    window.location="bottles.html";
+}
+
+function fruit_basket()
+{
+    window.location="fruit.html";
+}
